07fcd49d81564489e45fea9aab6d4861c5073f7b
Dudu Verderio <carlosbverderio@gmail.com>
2023-09-27 14:40:16 -0300

    commit app

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+
+  calcular(){
+
+    if(this.precoAlcool && this.precoGasolina){
+
+      let pAlcool = parseFloat(this.precoAlcool)
+      let pGasolina = parseFloat(this.precoGasolina)
+      let res = pAlcool / pGasolina
+
+      if(res >= 0.7){
+        this.resultado = "É melhor utilizar Gasolina!"
+      }
+      else{
+        this.resultado = "É melhor utilizar Álcool!"
+      }
+    }
+    else{
+      this.resultado = "Os campos não foram preenchidos corretamente"
+    }
+  }
+
+  precoAlcool = ""
+  precoGasolina = ""
+  public resultado: String = ""
+
+}
